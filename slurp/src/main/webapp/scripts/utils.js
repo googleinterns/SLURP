023b431372a9b2210fe74e93644d148ec0d8fff4
ananyay <ayammanuru@gmail.com>
2020-06-25 12:59:09 -0500

    Add tests for parseUrl and update JsDoc

--- slurp/src/main/webapp/scripts/utils.js
+++ slurp/src/main/webapp/scripts/utils.js
@@ -1,18 +1,18 @@
 /**
- * Parse URL from NAME.html?key1=value1&key2=value2... into {key1: value1, ...}
+ * Parse URL <pre><code>NAME.html?key1=value1&key2=value2...</code></pre> into {key1: value1, ...}.
  * @param {string} url The URL to be parsed. 
  * @returns {Object.<string, string>} Dictionary of the parsed URL parameters. 
  */
-function parseUrl(url) {
-  paramsUrl = url.substring(url.indexOf("?") + 1);
-  paramPairs = paramsUrl.split("&");
+export function parseUrl(url) {
+  var paramsUrl = url.substring(url.indexOf("?") + 1);
+  var paramPairs = paramsUrl.split("&");
   
   var params = {};
-  for (pair of paramPairs) {
-    splitIndex = pair.indexOf("=");
+  for (var pair of paramPairs) {
+    var splitIndex = pair.indexOf("=");
     
-    key = pair.substring(0, splitIndex);
-    value = pair.substring(splitIndex+1);
+    var key = pair.substring(0, splitIndex);
+    var value = pair.substring(splitIndex+1);
 
     params[key] = value;
   }
