1b26002cd443840b37caa896d31d3c121ef60976
Ananya Y <14322650+ananyeet@users.noreply.github.com>
2020-06-26 18:07:24 -0500

    Update sum.js to explain why the file exists.

--- slurp/src/test/sum.js
+++ slurp/src/test/sum.js
@@ -1,3 +1,6 @@
+/**
+ * This file exists to provide an example of how to use the Jest module. It has nothing to do with the functionality of the overall project.
+ */
 function sum(a, b) {
   return a + b;
 }
