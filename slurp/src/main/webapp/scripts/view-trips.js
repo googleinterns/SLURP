84de1857cdbfa384ec8c6848723d9ef50c1063f4
Zach Ghera <zpghera00@gmail.com>
2020-06-26 13:51:36 -0400

    Re-arranged and added documentation to each function.

--- slurp/src/main/webapp/scripts/view-trips.js
+++ slurp/src/main/webapp/scripts/view-trips.js
@@ -12,13 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * Temporary hardcoded function that returns the current users email.
+ *
+ * The hardcoded string was created based on one of the manually created test
+ * Trip Documents. This function will be implemented in the user authentication
+ * JS module using Firebase's Authentication API.
+ *
+ * TODO(Issue 16): Remove this function once implemented in authentication
+ *                 module.
+ */
+function getUserEmail() {
+  return 'matt.murdock';
+}
+
 
 /**
- * Fetches the previously entered comments from the server and inserts each
- * comment as a list item of the 'comments-thread-container' <ul> element.
+ * Creates an <div> element containing the HTML of an error message when the
+ * query in getTrips() fails to fetch the users trips.
  *
- * The number of comments displayed is determined by
- * getNumCommentstoDisplay().
+ * @param {string} errorMessage The error message that should be placed in the
+ *    created div element and displayed on the view-trips page.
+ * @return {HTMLDivElement} The div element containing the error message.
+ */
+function createErrorElement(errorMessage) {
+  const divElement = document.createElement('div');
+  const textElement = document.createElement('p');
+  textElement.innerText = errorMessage;
+  divElement.appendChild(textElement);
+
+  return divElement;
+}
+
+/**
+ * Creates an <div> element containing the HTML for an individual trip.
+ *
+ * Temporarily, only the title and associated document ID are included in a
+ * text element for each trip <div> element. This is done to simple test the
+ * query functionality.
+ *
+ * TODO(Issue 17): Feed all the Trip Doc data to the UI.
+ *
+ * @param {object} tripObj A JS object containg the fields (key value pairs) of
+ *    of a Trip Document.
+ * @param {string} tripId The Id associated with the current Trip Document.
+ * @return {HTMLDivElement} The div element containing the trip data.
+ */
+function createTripElement(tripObj, tripId) {
+  const tripElement = document.createElement('div');
+
+  const titleEl = document.createElement('p');
+  titleEl.innerText = `Title: ${tripObj.name} | Document Id: ${tripId}`;
+  tripElement.appendChild(titleEl);
+
+  return tripElement;
+}
+
+/**
+ * Queries, fetches, and serves all trips to the UI that the current user is a
+ * collaborator on.
+ *
+ * The current user's email is retrieved from getUserEmail in the Authentication
+ * module. Once all of the Trip documents are fetched, they are each passed to
+ * createTripElement() to create the HTML for each trip and appended to the
+ * trips-container <div>.
  */
 function getTrips() {
   const tripsContainer = document.getElementById('trips-container');
@@ -28,52 +85,15 @@ function getTrips() {
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          console.log(typeof doc.data());
-          console.log(doc.data());
-          tripsContainer.appendChild(createTripElement(doc.id));
+          console.log(typeof doc.id);
+          tripsContainer.appendChild(createTripElement(doc.data(), doc.id));
         });
       })
       .catch(error => {
-        console.log('Error in getCommentsThread: ' + error);
+        console.log(`Error in getCommentsThread: ${error}`);
         tripsContainer.appendChild(
             createErrorElement('Error: Unable to load your trips.'));
       })
-}
-
-function getUserEmail() {
-  return 'matt.murdock';
-}
-
-/**
- * Creates an <li> element containing the comment data.
- *
- * Each comment contains a message text, image, landmark name/location,
- * and landmark latitude-longitude coordinates. However, both the blobKey
- * and landmark fields of the comment JSON object can be null. If the
- * blobKey value null, no image element is included in the parent <li> element.
- * If the landmark value is null, no landmark information is included.
- *
- * @param {object} commentInJson A string that contains a JSON object of an
- *    individual comment. This JSON object contains fields for message text,
- *    image, landmark name, and landmark latitude-longitude coordinates.
- * @return {HTMLDivElement} The div element containing the trip data.
- */
-function createTripElement(tripId) {
-  const divElement = document.createElement('div');
-  const textElement = document.createElement('p');
-  textElement.innerText = tripId;
-  divElement.appendChild(textElement);
-
-  return divElement;
-}
-
-function createErrorElement(errorMessage) {
-  const divElement = document.createElement('div');
-  const textElement = document.createElement('p');
-  textElement.innerText = errorMessage;
-  divElement.appendChild(textElement);
-
-  return divElement;
 }
 
 /**
