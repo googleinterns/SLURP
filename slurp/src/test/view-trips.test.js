d6b19f4b09c98fb27256cf63da89e76187b855bc
Zach Ghera <zpghera00@gmail.com>
2020-06-29 16:18:47 -0400

    Add barebones view-trips test file.

---
+++ slurp/src/test/view-trips.test.js
@@ -0,0 +1,27 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const view_trips = require('../main/webapp/scripts/view-trips.js');
+
+test('User has no trips.', () => {
+});
+
+test('User has one trip.', () => {
+});
+
+test('User has multiple trips.', () => {
+  // const url = "test.html?key1=!#sJk3r2Iedy2dw[-cc2>&key2=value2";
+  // const urlParams = utils.parseUrl(url);
+  // expect(urlParams).toEqual({"key1": "!#sJk3r2Iedy2dw[-cc2>", "key2": "value2"});
+});
