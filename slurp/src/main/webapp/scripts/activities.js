9a32469ff5414f2fe6f9d3531eb1efb1bbcdcf23
ananyay <ayammanuru@gmail.com>
2020-06-29 15:25:49 -0500

    sort activities

--- slurp/src/main/webapp/scripts/activities.js
+++ slurp/src/main/webapp/scripts/activities.js
@@ -1,6 +1,9 @@
+const ACTIVITY_START_TIME = 'start_time';
+const ACTIVITY_END_TIME = 'end_time';
+
 /**
  * Make a dropdown object under the HTML element with ID #location,
- * with proviided title and HTML content.
+ * with provided title and HTML content.
  * @param {string} title The title of the to-be dropdown (what's displayed when the dropdown is closed).
  * @param {string} content The content of the to-be dropdown (what's displayed when the dropdown is open).
  * @param {string} location The parent HTML element of the to-be dropdown.
@@ -10,17 +13,17 @@ function makeDropdown(title, content, location) {
 
   const randomId = getRandomUuid();
 
-  let newButton = document.createElement("button");
+  let newButton = document.createElement('button');
   newButton.innerHTML = title;
-  newButton.setAttribute("data-toggle", "collapse");
-  newButton.setAttribute("data-parent", "#"+location);
-  newButton.setAttribute("href", "#"+randomId);
-  newButton.setAttribute("aria-expanded", "false");
+  newButton.setAttribute('data-toggle', 'collapse');
+  newButton.setAttribute('data-parent', '#'+location);
+  newButton.setAttribute('href', '#'+randomId);
+  newButton.setAttribute('aria-expanded', 'false');
 
-  let newDropdownContent = document.createElement("div");
+  let newDropdownContent = document.createElement('div');
   newDropdownContent.innerHTML = content;
-  newDropdownContent.classList.add("collapse");
-  newDropdownContent.setAttribute("tabindex", 0);
+  newDropdownContent.classList.add('collapse');
+  newDropdownContent.setAttribute('tabindex', 0);
   newDropdownContent.id = randomId;
 
   element.appendChild(newButton);
@@ -34,35 +37,54 @@ function makeDropdown(title, content, location) {
  * put in a dropdown.
  */
 function makeActivityContent(activityData) {
-  let content = "<p>";
-  content += ("Description: " + activityData["description"]);
-  content += ("<br>Start time: " + activityData["start_time"]);
-  content += ("<br>End time: " + activityData["end_time"]);
-  content += ("</p>");
+  let content = '<p>';
+  content += ('Description: ' + activityData['description']);
+  content += ('<br>Start time: ' + activityData['start_time']);
+  content += ('<br>End time: ' + activityData['end_time']);
+  content += ('</p>');
   return content;
+}
+
+/**
+ * Put a and b in display order. 
+ * @param {dictionary} a Dictionary representing activity a and its fields. 
+ * @param {dictionary} b Dictionary representing activity b and its fields.
+ */
+function compareActivities(a, b) {
+  if (a[ACTIVITY_START_TIME]['seconds'] < b[ACTIVITY_START_TIME]['seconds']) {
+    return -1;
+  } else if (a[ACTIVITY_START_TIME]['seconds'] > b[ACTIVITY_START_TIME]['seconds']) {
+    return 1;
+  } else if (a[ACTIVITY_END_TIME] > b[ACTIVITY_END_TIME]) {
+    return 1;
+  }
+  return -1;
 }
 
 /**
  * Fetch activities from the TripID provided in the URL and 
  * display the trips in #activity-list
  */
-function fetchAndDisplayActivities() {
+async function fetchAndDisplayActivities() {
   const url = window.location.href;
   const urlParams = parseUrl(url);
-  const tripId = urlParams["tripid"];
+  const tripId = urlParams['tripid'];
 
-  //TODO: CHANGE THIS INTO 1. AGGREGATE LIST, 2. MAKE DROPDOWN.
   db.collection(TRIP_COLLECTION).doc(tripId)
     .collection(ACTIVITY_COLLECTION).get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc){
-        const activityData = doc.data();
-        console.log(activityData);
-        const activityTitle = activityData["title"];
-        const activityContent = makeActivityContent(activityData);
-
-        makeDropdown(activityTitle, activityContent, "activity-list");
+      let tripActivities = [];
+      querySnapshot.forEach(function(doc) {
+        // If start date != end date, then split into 2 days and enter twice
+        let data = doc.data();
+        data['id'] = doc.id;
+        console.log(data[ACTIVITY_START_TIME]['seconds']);
+        tripActivities.push(data);
       });
+      return tripActivities;
     }).catch( function(error) {
-      console.log("Error getting trip details for tripId " + tripId);
+      console.log('Error getting trip details for tripId ' + tripId);
+    }).then( function(tripActivities) {
+      tripActivities = tripActivities.sort(compareActivities);
+      // split into days.
     });
 }
