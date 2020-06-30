bba04a5fe149373e838b5cd88a3c6348db507378
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-06-29 23:47:56 -0400

    Handle authentication error

--- slurp/src/main/webapp/scripts/login.js
+++ slurp/src/main/webapp/scripts/login.js
@@ -12,12 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * Handles the error returned by FirebaseUI when user login is unsuccessful.
+ *
+ * @param {firebaseui.auth.AuthUIError} error The error returned by FirebaseUI
+ * when user login is unsuccessful. See FirebaseUI documentation for error
+ * properties.
+ */
+function handleAuthError(error) {
+  // TODO: Issue #27, display a generic error message instead of printing to
+  // console.
+  console.log(error);
+}
+
 const firebaseUiConfig = {
   signInSuccessUrl: 'view-trips.html',
   signInOptions: [
     // TODO: Issue #26, incorporate other providers besides Google.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
+  ],
+  callbacks: {
+    // Called on unsuccessful sign-in.
+    signInFailure: function(error) {
+      return handleAuthError(error);
+    }
+  }
 };
 
 const firebaseAuthUi = new firebaseui.auth.AuthUI(firebase.auth());
