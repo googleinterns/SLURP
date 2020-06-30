4ae8b9ae073c06b50673de81b36aa3908a341301
Zach Ghera <zpghera00@gmail.com>
2020-06-29 20:36:11 -0400

    Run clang format on init-firebase.js.

--- slurp/src/main/webapp/scripts/init-firebase.js
+++ slurp/src/main/webapp/scripts/init-firebase.js
@@ -13,18 +13,14 @@
 // limitations under the License.
 
 
-// Initialize Firebase and its related product we use using the provided
-// Firebase project configuation.
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
 // TO USE THE EMULATOR: UPDATE THESE TWO VALUES.
-const USE_EMULATOR = true;
-const FIREBASE_EMULATOR_PORT = 8000; // This is the port from when you run firebase emulators:start.
+const USE_EMULATOR = false;
+// This is the port from when you run firebase emulators:start.
+const FIREBASE_EMULATOR_PORT = 9090;
 
-const RUN_LOCALLY = USE_EMULATOR && (location.hostname === "localhost" || location.hostname === "");
-const PROJECT_ID = "step53-2020";
+const RUN_LOCALLY = USE_EMULATOR &&
+    (location.hostname === 'localhost' || location.hostname === '');
+const PROJECT_ID = 'step53-2020';
 
 let firebaseConfig;
 if (RUN_LOCALLY) {
@@ -33,39 +29,36 @@ if (RUN_LOCALLY) {
   };
 } else {
   firebaseConfig = {
-    apiKey: "AIzaSyAAJgRhJY_rRn_q_On1HdA3hx15YHSkEJg",
-    authDomain: "step53-2020.firebaseapp.com",
-    databaseURL: "https://step53-2020.firebaseio.com",
+    apiKey: 'AIzaSyAAJgRhJY_rRn_q_On1HdA3hx15YHSkEJg',
+    authDomain: 'step53-2020.firebaseapp.com',
+    databaseURL: 'https://step53-2020.firebaseio.com',
     projectId: PROJECT_ID,
-    storageBucket: "step53-2020.appspot.com",
-    messagingSenderId: "905834221913",
-    appId: "1:905834221913:web:25e711f1132b2c0537fc48",
-    measurementId: "G-PLVY991DHW"
+    storageBucket: 'step53-2020.appspot.com',
+    messagingSenderId: '905834221913',
+    appId: '1:905834221913:web:25e711f1132b2c0537fc48',
+    measurementId: 'G-PLVY991DHW'
   };
 }
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
 if (RUN_LOCALLY) {
-  db.settings({
-    host: "localhost:" + FIREBASE_EMULATOR_PORT,
-    ssl: false
-  });
+  db.settings({host: 'localhost:' + FIREBASE_EMULATOR_PORT, ssl: false});
 }
 
-const TRIP_COLLECTION = "trips";
+const TRIP_COLLECTION = 'trips';
 
-const COLLABORATORS_FIELD = "collaborators";
-const DESCRIPTION_FIELD = "description";
-const DESTINATION_FIELD = "destination";
-const END_DATE_FIELD = "end_date";
-const START_DATE_FIELD = "start_date";
-const NAME_FIELD = "name";
+const COLLABORATORS_FIELD = 'collaborators';
+const DESCRIPTION_FIELD = 'description';
+const DESTINATION_FIELD = 'destination';
+const END_DATE_FIELD = 'end_date';
+const START_DATE_FIELD = 'start_date';
+const NAME_FIELD = 'name';
 
 /**
  * Export firestore instance and constants for use in view-trips.test.js.
  */
-if (typeof exports !== 'undefined'){
+if (typeof exports !== 'undefined') {
   module.exports.db = db;
 
   module.exports.TRIP_COLLECTION = TRIP_COLLECTION;
