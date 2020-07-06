591d2185bf706bc6394135c8b940dafc6a898aff
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-06 01:21:28 -0400

    Clean up backend

--- backend/src/main/java/com/google/slurp/servlets/DataServlet.java
+++ backend/src/main/java/com/google/slurp/servlets/DataServlet.java
@@ -20,13 +20,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+/**
+* Servlet that returns some example content.
+*/
+@WebServlet("/api/v1/test-servlet")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+ @Override
+ public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+   response.setContentType("text/html;");
+   response.getWriter().println("Hello world from data servlet!");
+ }
 }
+
