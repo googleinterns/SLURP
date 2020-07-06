7e4e5887832e751d40793f62a4185a92f13895a4
ananyay <ayammanuru@gmail.com>
2020-07-06 17:06:30 -0500

    Display activity list

--- frontend/src/components/ViewActivities/index.js
+++ frontend/src/components/ViewActivities/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import App from '../Firebase';
+import ActivityList from './activitylist';
+
 
 /**
  * ViewActivities component.
  */
-const ViewActivities = () => {
-  return (
-    <div>
-      <h1>View Activities</h1>
-    </div>
+const ViewActivities = (props) => {
+  return ( 
+    <ActivityList tripId={props.match.params.tripId}/>
   );
 };
 
