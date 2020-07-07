89af2a684a2b08fdf4ab62bbf4c85f7932585afe
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-07 12:56:05 -0400

    Remove auth code and change components from functional to class

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-
-import { SIGN_IN } from '../../constants/routes';
 
 /**
- * Landing component that defines the first page the user encounters in the
- * application.
+ * Landing component.
  */
-const Landing = () => {
-  const history = useHistory();
-
-  function handleClick() {
-    history.push(SIGN_IN);
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <Card className='text-center'>
-        <Card.Body>
-          <Card.Title>Welcome to SLURP</Card.Title>
-          {/* TODO (Issue #24): Put path to logo when we have one. */}
-          <img alt='SLURP Logo'></img>
-        </Card.Body>
-        <Button type='button' onClick={handleClick} variant='primary' size='lg'>
-          Sign In
-        </Button>
-      </Card>
-    </div>
-  );
 };
 
 export default Landing;
