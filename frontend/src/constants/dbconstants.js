9024a9969c11d9b34b8f19421b747018f8ef8455
ananyay <ayammanuru@gmail.com>
2020-07-07 15:01:58 -0500

    Add some relevant database constants.

---
+++ frontend/src/constants/dbconstants.js
@@ -0,0 +1,7 @@
+export const COLLECTION_TRIPS = "trips";
+export const COLLECTION_ACTIVITIES = "activities";
+
+export const ACTIVITIES_START_TIME = "start_time";
+export const ACTIVITIES_END_TIME = "end_time";
+export const ACTIVITIES_TITLE = "title";
+export const ACTIVITIES_DESCRIPTION = "description";
