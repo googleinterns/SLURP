cfb7b72e0955c1c304684a0f20a8fc06b8953923
ananyay <ayammanuru@gmail.com>
2020-07-07 10:54:13 -0500

    Refactor index.js into classes.

--- frontend/src/components/ViewActivities/index.js
+++ frontend/src/components/ViewActivities/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import App from '../Firebase';
 import ActivityList from './activitylist';
 
+class ViewActivities extends React.Component {
+  /** @inheritdoc */
+  constructor(props) {
+    super(props);
+    this.state = {tripId: props.match.params.tripId};
+  }
 
-/**
- * ViewActivities component.
- */
-const ViewActivities = (props) => {
-  return ( 
-    <ActivityList tripId={props.match.params.tripId}/>
-  );
-};
+  /** @inheritdoc */
+  render() {
+    return (
+      <div> <ActivityList tripId={this.state.tripId}/></div>
+    )
+  }
+}
 
 export default ViewActivities;
