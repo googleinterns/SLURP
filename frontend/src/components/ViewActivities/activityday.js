5ce7849279dd80f26dc5e6feb4911e315b80f3a2
ananyay <ayammanuru@gmail.com>
2020-07-07 16:50:40 -0500

    An activity day dropdown

---
+++ frontend/src/components/ViewActivities/activityday.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
+import Activity from './activity';
+
+
+class ActivityDay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: props.date, activities: Array.from(props.activities)};
+  }
+
+  render() {
+    let id = new Date(this.state.date).getTime();
+    return (
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          {this.state.date}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            {this.state.activities.map((activity, index) => (
+            <Activity activity={activity} key={index + this.state.date}/>
+            ))}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    );
+  }
+}
+
+export default ActivityDay;
