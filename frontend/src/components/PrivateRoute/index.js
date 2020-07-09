97bf09503fffc4fd648651c55e51543a201fd050
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-09 16:05:53 -0400

    Add PrivateRoute component

---
+++ frontend/src/components/PrivateRoute/index.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../Auth';
+
+import { SIGN_IN } from '../../constants/routes.js';
+
+/**
+ * PrivateRoute component that takes the authentication status of the current
+ * user from AuthContext. If they are authenticated, they will be allows to view
+ * the contents of the Route component. If they are not authenticated, they will
+ * be redirected to the SIGN_IN page.
+ */
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+  const {currentUser} = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      render={routeProps =>
+        currentUser ? <RouteComponent {...routeProps} />
+                    : <Redirect to={SIGN_IN} />
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
