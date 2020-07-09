052f25289c6f570cfcf634713a0cbde71f5d1adf
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-09 16:07:33 -0400

    Make some pages private routes

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { AuthProvider } from '../Auth';
+import PrivateRoute from '../PrivateRoute';
 
 import LandingPage from '../Landing';
 import SignInPage from '../SignIn'
@@ -14,14 +16,16 @@ import * as ROUTES from '../../constants/routes';
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.VIEW_TRIPS} component={ViewTripsPage} />
-          <Route path={ROUTES.VIEW_ACTIVITIES} component={ViewActivitiesPage} />
-        </div>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <div>
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <PrivateRoute path={ROUTES.VIEW_TRIPS} component={ViewTripsPage} />
+            <PrivateRoute path={ROUTES.VIEW_ACTIVITIES} component={ViewActivitiesPage} />
+          </div>
+        </Router>
+      </AuthProvider>
     );
   }
 };
