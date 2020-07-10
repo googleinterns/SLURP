5c8d8de7cfee65dc46cc761904a2a7b56e406c97
Zach Ghera <zpghera00@gmail.com>
2020-07-10 15:44:52 -0400

    Remove tests for trips. More detail added to PR descrip.

--- frontend/src/components/ViewTrips/trip.js
+++ frontend/src/components/ViewTrips/trip.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
 /**
- * Returns the title of the trip associated with the Trip document data
- * `tripObj`.
- *
- * @param {firebase.firestore.DocumentData} tripObj Object containing the fields
- *    and values for a Trip document.
- * @return Title of the trip (if it exists).
- */
-function createTitleElement(tripObj) {
-  try {
-    if('name' in tripObj) {
-      return tripObj.name;
-    }
-    throw new Error(`Property 'name' is not defined in 'tripObj.'`);
-  } catch (error) {
-    console.log(`Error in fetching trip title: ${error}`);
-    return 'Unable to fetch trip title';
-  }
-}
-
-/**
  * Component corresponding to the container containing an individual trip.
+ *
+ * Trip object fields are cleaned and vetted with firestore security rules
+ * when trips are added and/or editted. Thus, no error checking is done here
+ * on the 'display' side.
  *
  * Temporarily, only the title and associated document ID are included in a
  * text element for each trip <div> element. This is done to simple test the
@@ -32,11 +16,10 @@ function createTitleElement(tripObj) {
 const Trip = (props) => {
   return (
     <div>
-      <h2>{createTitleElement(props.tripObj)}</h2>
+      <h2>{createTitleElement(props.tripObj.name)}</h2>
       <p>Doc Id: {props.tripId}</p>
     </div>
   );
 };
 
 export default Trip;
-export { createTitleElement };
