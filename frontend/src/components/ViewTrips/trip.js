1df22599f10f86484a07807670e894bb748f8ea3
Zach Ghera <zpghera00@gmail.com>
2020-07-13 16:00:56 -0400

    Add simple test for getDateRange in trip.js.

--- frontend/src/components/ViewTrips/trip.js
+++ frontend/src/components/ViewTrips/trip.js
@@ -7,6 +7,8 @@ import ViewActivitiesButton from './view-activities-button.js';
  * Returns the date range of the trip associated with the Trip document data
  * `tripObj`.
  *
+ * Note: tripObj will always contain valid star_date and end_date fields.
+ *
  * @param {firebase.firestore.DocumentData} tripObj Object containing the fields
  *    and values for a Trip document.
  * @return Date range of the trip (if it exists).
---
+++ frontend/src/components/ViewTrips/trip.test.js
@@ -0,0 +1,21 @@
+import { getDateRange } from './trip.js'
+
+test('getDateRange test', () => {
+  const startMonth = 12; // December
+  const startDay = 17;
+  const startYear = 1995;
+  const endMonth = 5;    // May
+  const endDay = 24;
+  const endYear = 1996;
+  const testStartDate = firebase.firestore.Timestamp.fromDate(
+      new Date(startYear, startMonth - 1, startDay));
+  const testEndDate = firebase.firestore.Timestamp.fromDate(
+      new Date(endYear, endMonth - 1, endDay));
+  const testTripObj = {start_date: testStartDate, end_date: testEndDate};
+
+  const actualDateRange = getDateRange(testTripObj);
+  const expectedDateRange = `${startMonth}/${startDay}/${startYear} - ` +
+        `${endMonth}/${endDay}/${endYear}`;
+
+  expect(actualDateRange).toEqual(expectedDateRange);
+})
