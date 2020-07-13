dc7bb1beaa6de390eb4302de8b2f45a01fbff3ad
ananyay <14322650+ananyeet@users.noreply.github.com>
2020-07-13 12:40:10 -0500

    Test time zones into constants.

--- frontend/src/components/Utils/time.test.js
+++ frontend/src/components/Utils/time.test.js
@@ -1,5 +1,8 @@
 import * as utils from './utils';
 
+const TZ_CHICAGO = 'America/Chicago';
+const TZ_SINGAPORE = ;
+
 test('new york date timestamp format', () => {
   // Month parameter is zero indexed so it's actually the 10th month.
   const testDate = new Date(Date.UTC(2020, 9, 3, 14, 19, 4, 23)).getTime();
@@ -12,8 +15,8 @@ test('other date timestamp format', () => {
   const testDate = new Date(Date.UTC(2020, 7, 23, 2, 3, 2, 4)).getTime();
   const expectedCentral = 'Saturday, August 22, 2020';
   const expectedSingapore = 'Sunday, August 23, 2020';
-  const actualCentral = utils.timestampToDateFormatted(testDate, 'America/Chicago');
-  const actualSingapore = utils.timestampToDateFormatted(testDate, 'Asia/Singapore');
+  const actualCentral = utils.timestampToDateFormatted(testDate, TZ_CHICAGO);
+  const actualSingapore = utils.timestampToDateFormatted(testDate, TZ_SINGAPORE);
   expect(actualCentral).toEqual(expectedCentral);
   expect(actualSingapore).toEqual(expectedSingapore);
 })
@@ -30,8 +33,8 @@ test('other time timestamp format', () => {
   const testDate = new Date(Date.UTC(2020, 7, 23, 2, 3, 2, 4)).getTime();
   const expectedCentral = '9:03 PM';
   const expectedSingapore = '10:03 AM';
-  const actualCentral = utils.timestampToTimeFormatted(testDate, 'America/Chicago');
-  const actualSingapore = utils.timestampToTimeFormatted(testDate, 'Asia/Singapore');
+  const actualCentral = utils.timestampToTimeFormatted(testDate, TZ_CHICAGO);
+  const actualSingapore = utils.timestampToTimeFormatted(testDate, TZ_SINGAPORE);
   expect(actualCentral).toEqual(expectedCentral);
   expect(actualSingapore).toEqual(expectedSingapore);
 })
