b3218136e510c91eee5980ec2f50f3fac7a45baf
Zach Ghera <zpghera00@gmail.com>
2020-07-13 18:46:53 -0400

    Add comment to clarify usage of date consts in getDateRange test.

--- frontend/src/components/ViewTrips/trip.test.js
+++ frontend/src/components/ViewTrips/trip.test.js
@@ -4,21 +4,22 @@ import 'firebase/firebase-firestore';
 import { getDateRange } from './trip.js'
 
 test('getDateRange test', () => {
+  // Dates used for both test and expected date strings
   const startMonth = 12;
   const startDay = 17;
   const startYear = 1995;
   const endMonth = 5;
   const endDay = 24;
   const endYear = 1996;
+
   const testStartDate = firebase.firestore.Timestamp.fromDate(
       new Date(startYear, startMonth - 1, startDay));
   const testEndDate = firebase.firestore.Timestamp.fromDate(
       new Date(endYear, endMonth - 1, endDay));
   const testTripObj = {start_date: testStartDate, end_date: testEndDate};
-
   const actualDateRange = getDateRange(testTripObj);
-  const expectedDateRange = `${startMonth}/${startDay}/${startYear} - ` +
-        `${endMonth}/${endDay}/${endYear}`;
 
-  expect(actualDateRange).toEqual(expectedDateRange);
+  const testDateRange = `${startMonth}/${startDay}/${startYear} - ` +
+        `${endMonth}/${endDay}/${endYear}`;
+  expect(testDateRange).toEqual(expectedDateRange);
 })
