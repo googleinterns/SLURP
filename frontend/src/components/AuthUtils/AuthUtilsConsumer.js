61d3e8d2bb3d7ef1650059b691d1ce3216403be6
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-12 23:25:59 -0400

    Add documentation

--- frontend/src/components/AuthUtils/AuthUtilsConsumer.js
+++ frontend/src/components/AuthUtils/AuthUtilsConsumer.js
@@ -4,7 +4,13 @@ import { AuthContext } from '../Auth';
 let currentUser;
 
 /**
- * TODO: document!
+ * React Context Consumer component that only gets the value of the current
+ * authenticated user as given by the AuthProvider component. Intended to be
+ * wrapped around the App component's Router component as a child of the
+ * AuthProvider component.
+ *
+ * @param {Object} props No arguments are expected for this component's props.
+ * The child components are simply passed through.
  */
 export const AuthUtilsConsumer = (props) => {
   currentUser = useContext(AuthContext);
@@ -13,6 +19,12 @@ export const AuthUtilsConsumer = (props) => {
   );
 };
 
+/**
+ * Returns the current authenticated user set by the AuthUtilsConsumer
+ * component.
+ *
+ * @returns {firebase.User} The currently authenticated user.
+ */
 export function getCurrentUser() {
   return currentUser;
 }
--- frontend/src/components/AuthUtils/index.js
+++ frontend/src/components/AuthUtils/index.js
@@ -1,13 +1,19 @@
 /**
- * TODO: Write description! Talk about how these functions must only be called
- * from inside a private route component.
+ * Utility functions to get various parameters corresponding to the currently
+ * authenticated user. These functions should ONLY be called from private pages,
+ * i.e. the user must be authenticated to ensure the parameters can be
+ * retrieved.
  */
 
 import { AuthUtilsConsumer, getCurrentUser } from './AuthUtilsConsumer.js';
 import { SIGN_IN } from '../../constants/routes.js';
 
 /**
- * TODO: document!
+ * Called when one of the authentication utility functions are used when the
+ * user has not logged in yet. Since the utility functions require that they be
+ * called from private pages, this function being called implies that the user
+ * managed to access a private page without being authenticated first. So they
+ * are immiedately redirected to the SIGN_IN page.
  */
 function redirectToSignIn() {
   window.location.href = SIGN_IN;
