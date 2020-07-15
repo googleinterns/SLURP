08440884706cc7b428fd5c90fbd6a818d92a46bf
Zach Ghera <zpghera00@gmail.com>
2020-07-14 21:49:34 -0400

    Comment in trip.test about why month numbers are decrementedby 1.

--- frontend/src/components/ViewTrips/trip.test.js
+++ frontend/src/components/ViewTrips/trip.test.js
@@ -4,7 +4,7 @@ import 'firebase/firebase-firestore';
 import { getDateRange } from './trip.js'
 
 test('getDateRange test', () => {
-  // Dates used for both test and expected date strings
+  // Dates used for both test and expected date strings.
   const startMonth = 12;
   const startDay = 17;
   const startYear = 1995;
@@ -12,14 +12,16 @@ test('getDateRange test', () => {
   const endDay = 24;
   const endYear = 1996;
 
+  // Note that the months in JS dates are 0 indexed rather than 1 indexed so
+  // they must be decremented in order for the month to be correct.
   const testStartDate = firebase.firestore.Timestamp.fromDate(
       new Date(startYear, startMonth - 1, startDay));
   const testEndDate = firebase.firestore.Timestamp.fromDate(
       new Date(endYear, endMonth - 1, endDay));
   const testTripObj = {start_date: testStartDate, end_date: testEndDate};
-  const actualDateRange = getDateRange(testTripObj);
+  const testDateRange = getDateRange(testTripObj);
 
-  const testDateRange = `${startMonth}/${startDay}/${startYear} - ` +
+  const expectedDateRange = `${startMonth}/${startDay}/${startYear} - ` +
         `${endMonth}/${endDay}/${endYear}`;
   expect(testDateRange).toEqual(expectedDateRange);
 })
