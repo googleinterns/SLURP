7ddc9e1bad4aaa41e273e7eeec8edd24b729f3bd
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-16 16:46:40 -0400

    Start editing index.js of ViewActivities

--- frontend/src/components/ViewActivities/index.js
+++ frontend/src/components/ViewActivities/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import app from '../Firebase';
 import ActivityList from './activitylist.js';
+
+import * as ROUTES from '../../constants/routes.js';
+import * as DB from '../../constants/database.js';
 
 /**
  * The whole view activities page.
- * 
+ *
  * @param {Object} props This component expects the following props:
- * - `tripId` {string} The trip's ID. This is sent to the component through the URL. 
+ * - `tripId` {string} The trip's ID. This is sent to the component through the URL.
  */
 class ViewActivities extends React.Component {
+  /** @inheritdoc */
+  constructor(props) {
+    super(props);
+    this.tripId = props.match.params.tripId;
+    this.state = { collaborators: undefined }
+  }
+
+  /** @inheritdoc */
+  componentDidMount() {
+    app.firestore()
+        .collection(DB.COLLECTION_TRIPS)
+        .doc(this.tripId)
+        .get()
+        .then(doc => {
+          this.setState({ collaborators: doc.get(DB.TRIPS_COLLABORATORS) });
+        });
+  }
+
+  /** @inheritdoc */
   render() {
     return (
-      <div className='activity-page'> 
-        <ActivityList tripId={this.props.match.params.tripId}/>
+      <div className='activity-page'>
+        <ActivityList tripId={this.tripId}/>
       </div>
     )
   }
