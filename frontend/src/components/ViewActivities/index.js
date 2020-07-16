c68ae012a35ea60ed3523e85820208a0b3fc524a
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-16 16:06:28 -0400

    Merge latest index.js of ViewActivities

--- frontend/src/components/ViewActivities/index.js
+++ frontend/src/components/ViewActivities/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import ActivityList from './activitylist.js';
 
 /**
- * ViewActivities component.
+ * The whole view activities page.
+ * 
+ * @param {Object} props This component expects the following props:
+ * - `tripId` {string} The trip's ID. This is sent to the component through the URL. 
  */
 class ViewActivities extends React.Component {
   render() {
     return (
-      <div>
-        <h1>View Activities</h1>
+      <div className='activity-page'> 
+        <ActivityList tripId={this.props.match.params.tripId}/>
       </div>
-    );
+    )
   }
-};
+}
 
 export default ViewActivities;
