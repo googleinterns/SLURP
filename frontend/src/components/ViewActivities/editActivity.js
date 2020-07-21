8ce13f0df8caa1330c08bc674e8b02641b00f64b
Ananya Y <14322650+ananyeet@users.noreply.github.com>
2020-07-21 12:07:43 -0500

    basic modal infrastructure with add activity capability

--- frontend/src/components/ViewActivities/editActivity.js
+++ frontend/src/components/ViewActivities/editActivity.js
@@ -108,7 +108,7 @@ class EditActivity extends React.Component {
     }
 
     return (
-      <FormControl as='select'
+      <Form.Control as='select'
         ref={st === 'start' ? this.editStartTzRef : this.editEndTzRef}
         key={st === 'start' ? this.state.startTz : this.state.endTz}
         defaultValue={defaultTz}
@@ -116,7 +116,7 @@ class EditActivity extends React.Component {
         {timezones.map((item, index) => {
           return (<option key={index}>{item}</option>);
         })}
-      </FormControl>
+      </Form.Control>
     )
   }
 
@@ -132,11 +132,11 @@ class EditActivity extends React.Component {
    */
   countriesDropdown(ref, tzref, defaultCountry) {
     return (
-      <FormControl as='select' ref={ref} onChange={tzref} defaultValue={defaultCountry}>
+      <Form.Control as='select' ref={ref} onChange={tzref} defaultValue={defaultCountry}>
         {countryList.map((item, index) => {
           return (<option key={index}>{item}</option>);
         })}
-      </FormControl>
+      </Form.Control>
     );
   }
 
--- frontend/src/components/ViewActivities/index.js
+++ frontend/src/components/ViewActivities/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ActivityList from './activitylist.js';
+import EditActivity from './editActivity.js';
+import { Button, Modal } from 'react-bootstrap';
+import app from '../Firebase'; 
+import * as DB from '../../constants/database.js';
+import { firestore } from 'firebase';
+
+const db = app.firestore();
 
 /**
  * The whole view activities page.
@@ -8,12 +15,51 @@ import ActivityList from './activitylist.js';
  * - `tripId` {string} The trip's ID. This is sent to the component through the URL. 
  */
 class ViewActivities extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { addingActivity : false }
+  }
+
+  /**
+   * Complete "Add Activity" operation
+   */
+  addActivity = (e) => {
+    e.preventDefault();
+    this.setState({ addingActivity: true });
+  }
+
+  doneAddingActivity = () => { this.setState({ addingActivity: false }) };
+
+  emptyActivity = (tripId) => {
+    const data = { 'fillerstamp' : firestore.Timestamp.now() };
+    db.collection(DB.COLLECTION_TRIPS).doc(tripId)
+      .collection(DB.COLLECTION_ACTIVITIES).add(data);
+    return data;
+  }
+
   render() {
-    return (
-      <div className='activity-page'> 
-        <ActivityList tripId={this.props.match.params.tripId}/>
-      </div>
-    )
+    const tripId = this.props.match.params.tripId;
+    if (!this.state.addingActivity) {
+      return (
+        <div>
+          <div className='activity-page'> 
+            <ActivityList tripId={tripId}/>
+          </div>
+          <Button type='button' onClick={this.addActivity}>+ Add</Button>
+        </div>
+      );
+    } else {
+      return (
+        <Modal.Dialog>
+          <Modal.Header>Add New Activity</Modal.Header>
+          <Modal.Body>
+            <EditActivity activity={this.emptyActivity(tripId)} 
+              submitFunction={this.doneAddingActivity}/> 
+          </Modal.Body>
+        </Modal.Dialog>
+      );
+    }
   }
 }
 
