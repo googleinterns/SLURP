9f0fe8a112d8fd708b12ea2e710ffd51366d1710
Zach Ghera <zpghera00@gmail.com>
2020-07-22 13:48:26 -0400

    Complete initial integration of onSnapshot listener for real-time updates.

--- frontend/src/components/ViewTrips/trips-container.js
+++ frontend/src/components/ViewTrips/trips-container.js
@@ -9,51 +9,6 @@ import Trip from './trip.js';
 const db = app.firestore();
 
 /**
- * Returns a promise of a query object containg the array of Trip Documents
- * corresponding to the trips that the current user is a collaborator on.
- *
- * @param {firebase.firestore.Firestore} db The Firestore database instance.
- * @return {Promise<!firebase.firestore.QuerySnapshot>} Promise object
- *    containing the query results with zero or more Trip  documents.
- */
-function queryUserTrips(db) {
-  const curUserUid = getCurUserUid();
-  return db.collection(DB.COLLECTION_TRIPS)
-      .where(DB.TRIPS_COLLABORATORS, 'array-contains', curUserUid)
-      .orderBy(DB.TRIPS_CREATION_TIME, 'desc')
-      .get();
-}
-
-/**
- * Grabs Trips query result from `queryUserTrips()` and returns an array of
- * `<Trip>` elements as defined in `trip.js`.
- *
- * @param {Promise<!firebase.firestore.QuerySnapshot>} querySnapshot Promise
- *     object containing the query results with zero or more Trip documents.
- * @param {EventHandler} handleEditTrip Displays the edit trip modal.
- * @param {EventHandler} refreshTripsContainer Refreshed the TripsContainer
- *     component (Remove when fix Issue #62).
- * @return {Promise<!Array<Trip>>} Promise object containing an array
- *     of Trip React/HTML elements corresponding to the Trip documents included
- *     in `querySnapshot`.
- */
-function serveTrips(querySnapshot, handleEditTrip, refreshTripsContainer) {
-  return new Promise(function(resolve) {
-    const tripsContainer = querySnapshot.docs.map(doc =>
-        ( <Trip
-            tripData={doc.data()}
-            tripId={doc.id}
-            handleEditTrip={handleEditTrip}
-            refreshTripsContainer={refreshTripsContainer}
-            key={doc.id}
-          />
-        )
-    );
-    resolve(tripsContainer);
-  });
-}
-
-/**
  * Returns a `<div>` element with the specified error message.
  *
  * @param {!string} error Error message in `componentDidMount` catch statement.
@@ -73,10 +28,6 @@ function getErrorElement(error) {
  *
  * @param {Object} props These are the props for this component:
  * - handleEditTrip: Handler that displays the edit trip modal.
- * - refreshTripsContainer: Handler that refreshes the TripsContainer
- *        component upon trip creation (Remove when fix Issue #62).
- * - key: Special React attribute that ensures a new TripsContainer instance is
- *        created whenever this key is updated (Remove when fix Issue #62).
  * @extends React.Component
  */
 class TripsContainer extends React.Component {
@@ -86,19 +37,32 @@ class TripsContainer extends React.Component {
     this.state = {trips: []};
   }
 
+  serveUserTrips = () => {
+    const curUserUid = getCurUserUid();
+    db.collection(DB.COLLECTION_TRIPS)
+        .where(DB.TRIPS_COLLABORATORS, 'array-contains', curUserUid)
+        .orderBy(DB.TRIPS_CREATION_TIME, 'desc')
+        .onSnapshot(querySnapshot => {
+          const tripsContainer = querySnapshot.docs.map(doc =>
+              ( <Trip
+                  tripData={doc.data()}
+                  tripId={doc.id}
+                  handleEditTrip={this.props.handleEditTrip}
+                  key={doc.id}
+                />
+              )
+          );
+
+          this.setState({ trips: tripsContainer });
+        }, (error) => {
+          let errorElement = getErrorElement(error);
+          this.setState({ trips: errorElement });
+        });
+  }
+
   /** @inheritdoc */
   async componentDidMount() {
-    try {
-      const querySnapshot = await queryUserTrips(db);
-      let tripsContainer = await serveTrips(querySnapshot,
-                                            this.props.handleEditTrip,
-                                            this.props.refreshTripsContainer);
-      this.setState({ trips: tripsContainer });
-    }
-    catch (error) {
-      let errorElement = await getErrorElement(error);
-      this.setState({ trips: errorElement });
-    }
+    this.serveUserTrips();
   }
 
   /** @inheritdoc */
