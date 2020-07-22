9e1b699ed743216a09a5adaca61950c29e96e156
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-22 13:20:57 -0400

    Add WebListener for the backend server

---
+++ backend/src/main/java/com/google/slurp/servlets/ServerListener.java
@@ -0,0 +1,47 @@
+package com.google.slurp.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+/**
+ * Servlet context listener that is looked for whenever the backend server
+ * receives a startup or shutdown request.
+ */
+@WebListener
+public class ServerListener implements ServletContextListener {
+
+  /**
+   * {@inheritDoc}
+   *
+   * Initialize the Firebase Admin app for the server.
+   */
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    System.out.println("Server initialized and starting up...");
+
+    try {
+      FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredentials(GoogleCredentials.getApplicationDefault())
+          .build();
+
+      FirebaseApp.initializeApp(options);
+    } catch (IOException error) {
+      System.err.println("Error obtaining the Application Default Credentials:");
+      error.printStackTrace();
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    System.out.println("Server destroyed and shutting down...");
+  }
+}
