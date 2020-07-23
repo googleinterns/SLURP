1fd1f157d9a2605bf7b02a290adbf9ecdd6b98ea
Zach Ghera <zpghera00@gmail.com>
2020-07-23 12:53:59 -0400

    Update timestamp and trip title fields to better naming convention.

--- frontend/src/constants/database.js
+++ frontend/src/constants/database.js
@@ -2,13 +2,13 @@
  * This file specifies the database collection and field names.
  */
 export const COLLECTION_TRIPS = 'trips';
-export const TRIPS_NAME = 'name';
+export const TRIPS_TITLE = 'title';
 export const TRIPS_DESCRIPTION = 'description';
 export const TRIPS_DESTINATION = 'destination';
 export const TRIPS_COLLABORATORS = 'collaborators';
 export const TRIPS_START_DATE = 'start_date';
 export const TRIPS_END_DATE = 'end_date';
-export const TRIPS_CREATION_TIME = 'trip_creation_time';
+export const TRIPS_UPDATE_TIMESTAMP = 'trip_update_timestamp';
 
 export const COLLECTION_ACTIVITIES = 'activities';
 export const ACTIVITIES_START_TIME = 'start_time';
