378a3cf87e5736bfbad1fc7b3d3e7977c6c7e909
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-07-23 03:05:29 -0400

    Implemented converting emails to UIDs

--- backend/src/main/java/com/google/slurp/servlets/ConvertEmailsToUidsServlet.java
+++ backend/src/main/java/com/google/slurp/servlets/ConvertEmailsToUidsServlet.java
@@ -15,23 +15,119 @@
 package com.google.slurp.servlets;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.firebase.auth.EmailIdentifier;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.GetUsersResult;
+import com.google.firebase.auth.UserIdentifier;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
- * Servlet that converts a given POST request of user emails to the corresponding user UIDs, sending
- * this back as the response.
+ * Servlet that converts a given POST request of user emails to the
+ * corresponding user UIDs, sending this back as the response.
  */
 @WebServlet("/api/v1/convert-emails-to-uids")
 public class ConvertEmailsToUidsServlet extends HttpServlet {
+  private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+  /**
+   * Constructs a list of <code>UserIdentifier</code> using the given JSON array
+   * supplied by the POST request.
+   *
+   * @param json The body of the POST request, expected to be a String array of
+   *             user emails.
+   * @return A list of UserIdentifier objects identified by user email. Can then
+   *         be used in a <code>FirebaseAuth</code> instance's
+   *         <code>getUsers</code> or <code>getUsersAsync</code> functions.
+   */
+  private List<UserIdentifier> getUserIdentifiers(Reader json) {
+    Type typeOfListString = new TypeToken<List<String>>() {
+    }.getType();
+    List<String> userEmails = new Gson().fromJson(json, typeOfListString);
+    return userEmails.stream().map(email -> new EmailIdentifier(email)).collect(Collectors.toList());
+  }
+
+  /**
+   * Given a list of emails that are not registered with SLURP, create user accounts for each of
+   * them and return the corresponding list of Firebase-generated user UIDs.
+   *
+   * @param emails The list of emails to create users for in Firebase Auth.
+   * @return The list of Firebase-generated user UIDs from the created users.
+   * @throws FirebaseAuthException If an error occurs when creating a user.
+   */
+  private List<String> createUsersAndGetUids(List<String> emails) throws FirebaseAuthException {
+    List<CreateRequest> requests = emails.stream()
+                                         .map(email -> new CreateRequest().setEmail(email))
+                                         .collect(Collectors.toList());
+
+    List<String> createdUids = new ArrayList<String>();
+    for (CreateRequest request : requests) {
+      UserRecord userRecord = auth.createUser(request);
+      createdUids.add(userRecord.getUid());
+    }
+
+    return createdUids;
+  }
 
   /**
    * {@inheritDoc}
+   *
+   * Given a JSON array of user emails, sends back to the response a JSON array of the corresponding
+   * user UIDs. On error, sends an empty response.
    */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    List<UserIdentifier> userIdentifiers = getUserIdentifiers(request.getReader());
 
+    GetUsersResult result;
+    try {
+      result = auth.getUsersAsync(userIdentifiers)
+                   .get();
+    } catch (InterruptedException | ExecutionException error) {
+      System.err.println("Error when asynchronously getting users:");
+      error.printStackTrace();
+      return;
+    }
+    List<String> userUids = result.getUsers()
+                                  .stream()
+                                  .map(user -> user.getUid())
+                                  .collect(Collectors.toList());
+    List<String> remainingEmails = result.getNotFound()
+                                         .stream()
+                                         .map(user ->
+                                           StringUtils.substringBetween(user.toString(), "(", ")")
+                                         )
+                                         .collect(Collectors.toList());
+    List<String> remainingUids;
+    try {
+      remainingUids = createUsersAndGetUids(remainingEmails);
+    } catch (FirebaseAuthException error) {
+      System.err.println("Error creating new users:");
+      error.printStackTrace();
+      return;
+    }
+
+    List<String> uidsToReturn = Stream.concat(userUids.stream(), remainingUids.stream())
+                                      .collect(Collectors.toList());
+    response.setContentType("application/json");
+    response.getWriter().println(new Gson().toJson(uidsToReturn));
   }
 }
