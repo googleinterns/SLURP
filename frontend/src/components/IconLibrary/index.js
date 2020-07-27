223829c4a6befb18d2ea6d2b9c71ba59f3a94c80
Zach Ghera <zpghera00@gmail.com>
2020-07-27 13:59:38 -0400

    Add hiking icon for view activities (temporary solution for now).

--- frontend/src/components/IconLibrary/index.js
+++ frontend/src/components/IconLibrary/index.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faMapMarkerAlt, faCalendarAlt, faBook, faUserFriends,
+import { faMapMarkerAlt, faCalendarAlt, faBook, faUserFriends, faHiking,
          faTrash, faEdit  } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
@@ -8,6 +8,7 @@ library.add(
   faCalendarAlt,
   faBook,
   faUserFriends,
+  faHiking,
   faTrash,
   faEdit,
 );
