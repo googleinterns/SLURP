933a93f34155ce8d363f21afc1816ea07cff3a21
Zach Ghera <zpghera00@gmail.com>
2020-07-30 12:12:08 -0400

    Add top level comment that explains need for IconsLibrary index.js.

--- frontend/src/components/IconLibrary/index.js
+++ frontend/src/components/IconLibrary/index.js
@@ -1,3 +1,8 @@
+/**
+ * This file defines all icons used in the project. This is the recommended
+ * method to import icons based on the documentation here:
+ * https://github.com/FortAwesome/react-fontawesome#build-a-library-to-reference-icons-throughout-your-app-more-conveniently.
+ */
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { faMapMarkerAlt, faCalendarAlt, faBook, faUserFriends, faHiking,
