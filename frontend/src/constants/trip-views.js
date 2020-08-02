0d7bae045c164e65f35b4cae362c6275aa911454
Zach Ghera <zpghera00@gmail.com>
2020-08-02 16:32:38 -0400

    File that defined trip view enum.

---
+++ frontend/src/constants/trip-views.js
@@ -0,0 +1,14 @@
+/**
+ * Types of trip 'views' on the view trips page. Added for the trip invites feature.
+ * @enum {number}
+ */
+const TripViews = {
+  /** Displays trips that current user has created or accepted. */
+  ACTIVE = 0,
+  /** Displays trips that current user has not yet accepted or rejected. */
+  PENDING = 1,
+  /** Displays trips that current user has rejected. */
+  REJECTED = 2,
+};
+
+export default TripViews;
