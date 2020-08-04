338d5f594dd02c668de10eadffe835a9a2d79198
Zach Ghera <zpghera00@gmail.com>
2020-08-04 12:24:57 -0400

    Add saveTripModal messages to messages.js.

--- frontend/src/constants/messages.js
+++ frontend/src/constants/messages.js
@@ -1,3 +1,10 @@
-// Default values for form fields. 
+// Default and placeholder values for form fields.
 export const ACTIVITY_TITLE_PLACEHOLDER = 'What will you be doing?';
 export const ACTIVITY_DESCRIPTION_PLACEHOLDER = 'Add some details!';
+
+export const TRIPS_TITLE_DEFAULT = 'Untitled Trip';
+export const TRIPS_DESTINATION_DEFAULT = 'No Destination';
+export const TRIPS_TITLE_PLACEHOLDER = 'Title your trip!';
+export const TRIPS_DESCRIPTION_PLACEHOLDER = 'Add some details!';
+export const TRIPS_DESTINATION_PLACEHOLDER = 'Where will you be going?';
+export const TRIPS_COLLAB_EMAIL_PLACEHOLDER = 'person@email.xyz';
